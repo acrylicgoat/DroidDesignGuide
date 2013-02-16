53719a4ec5d445789b585c40ccb0dc69fb4fdcd8
acrylicgoat <acrylicgoat@gmail.com>
2013-02-16 09:26:16 -0600

    changed if statement to switch

--- src/com/acrylicgoat/droiddesign/activity/SublistActivity.java
+++ src/com/acrylicgoat/droiddesign/activity/SublistActivity.java
@@ -98,16 +98,19 @@ public class SublistActivity extends SherlockFragmentActivity implements DDGList
     @Override
     public boolean onOptionsItemSelected(MenuItem item) 
     {
-        if(item.getItemId() == R.id.ddg)
+        int itemId = item.getItemId();
+        switch (itemId)
         {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com/design/index.html"));
-            startActivity(browserIntent);
-        }
-        else if(item.getItemId() == android.R.id.home)
-        {
-            Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
-            mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(mainIntent);
+            case R.id.ddg:
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com/design/index.html"));
+                startActivity(browserIntent);
+                break;
+            case android.R.id.home:
+                Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+               mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+               startActivity(mainIntent);
+            default:
+                break;
         }
         return true;
     }
